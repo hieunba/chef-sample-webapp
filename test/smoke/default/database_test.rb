e43ed7f642025c0faacc3cc11ba6467557c6d1c2
Nghiem Ba Hieu <hi3unb@gmail.com>
2017-07-29 07:29:03 +0700

    Default tests for database recipe

---
+++ test/smoke/default/database_test.rb
@@ -0,0 +1,13 @@
+# InSpec test
+describe package('mysql-server-5.5') do
+    it { should be_installed }
+end
+
+describe service('mysql-default') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port 3306 do
+  it { should be_listening }
+end
