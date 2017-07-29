842d48ba53097e83059144623cc789adeafbbca4
Nghiem Ba Hieu <hi3unb@gmail.com>
2017-07-30 00:22:31 +0700

    Add more InSpec tests

--- test/smoke/default/database_test.rb
+++ test/smoke/default/database_test.rb
@@ -8,6 +8,16 @@ describe service('mysql-default') do
   it { should be_running }
 end
 
+describe mysql_conf('/etc/mysql-default/my.cnf').params('mysqld') do
+  its('port') { should eq '3306' }
+  its('socket') { should eq '/run/mysql-default/mysqld.sock' }
+end
+
 describe port 3306 do
   it { should be_listening }
+  its('protocols') { should include('tcp') }
+end
+
+describe command("mysql -uroot -pfakeroot -h 127.0.0.1 -s -e 'show databases;'") do
+  its('stdout') { should match(/mysql/) }
 end
